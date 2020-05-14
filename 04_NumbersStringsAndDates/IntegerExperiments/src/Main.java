561437c780037f5acafe6c06d9952771a6f2e845
Nikita <nikitosh1129_96@mail.ru>
2020-05-14 11:33:53 +0500

    first try

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,31 @@
 public class Main
 {
+
     public static void main(String[] args)
     {
-        Container container = new Container();
-        container.count += 7843;
-
+//        Container container = new Container();
+//        container.count += 7843;
+        Integer xx = sumDigits(115111);
+        System.out.println(xx);
     }
 
-    public Integer sumDigits(Integer number)
+    public static Integer sumDigits(Integer number)
     {
-        //@TODO: write code here
-        return 0;
+        int sum = 0;
+        int x = Integer.toString(number).length() - 1;
+        String text = Integer.toString(number);
+
+        while (0 <= x)
+        {
+
+            char c = text.charAt(x);
+            String g = String.valueOf(c);
+            Integer t = Integer.parseInt(g);
+            sum = sum + t;
+            x--;
+        }
+
+        return sum;
+
     }
 }
