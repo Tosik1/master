88964cf8b4d9f778842e1f6d095b769e214c1c62
Nikita <nikitosh1129_96@mail.ru>
2020-05-15 00:58:20 +0500

    module 4.2

---
+++ 04_NumbersStringsAndDates/Main/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Min value Byte: " + Byte.MIN_VALUE + ", max value Byte: " + Byte    .MAX_VALUE);
+        System.out.println("Min value Integer: " + Integer.MIN_VALUE + ", max value Integer: " + Integer.MAX_VALUE);
+        System.out.println("Min value Short: " + Short.MIN_VALUE + ", max value Short: " + Short.MAX_VALUE);
+        System.out.println("Min value Long: " + Long.MIN_VALUE + ", max value Long: " + Long.MAX_VALUE);
+        System.out.println("Min value Float: " + Float.MIN_VALUE + ", max value Float: " + Float.MAX_VALUE);
+        System.out.println("Min value Double: " + Double.MIN_VALUE + ", max value Double: " + Double.MAX_VALUE);
+    }
+}
