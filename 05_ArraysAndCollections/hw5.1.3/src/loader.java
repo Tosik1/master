e67e21314086a788ba248b206928d5c2c4c06709
Nikita <nikitosh1129_96@mail.ru>
2020-05-30 12:18:20 +0500

    hw5.1.3

--- 05_ArraysAndCollections/hw5.1.3/src/loader.java
+++ 05_ArraysAndCollections/hw5.1.3/src/loader.java
@@ -1,23 +1,24 @@
+import java.util.Scanner;
+
 public class loader
 {
     public static void main(String args[])
     {
-        String[][] crest = {
-                {"x", " ", " ", " ", " ", " ", "x"},
-                {" ", "x", " ", " ", " ", "x", " "},
-                {" ", " ", "x", " ", "x", " ", " "},
-                {" ", " ", " ", "x", " ", " ", " "},
-                {" ", " ", "x", " ", "x", " ", " "},
-                {" ", "x", " ", " ", " ", "x", " "},
-                {"x", " ", " ", " ", " ", " ", "x"}
-        };
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите нечетный номер строк и столбцов: ");
+        int n = in.nextInt();
+
+        String[][] crest = new String[n][n];
 
         for (int i = 0; i < crest.length; i++)
         {
             System.out.println();
             for (int j = 0; j < crest[i].length; j++)
             {
-                System.out.print(crest[i][j]);
+                if (i == j || i + j == (n - 1))
+                    System.out.print(crest[i][j] = "x");
+                else
+                    System.out.print(crest[i][j] = " ");
             }
         }
     }
