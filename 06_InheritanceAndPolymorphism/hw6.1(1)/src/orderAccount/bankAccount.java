cf47f4fa6896b985c0dda95bc532a0c536f31c86
Nikita <nikitosh1129_96@mail.ru>
2020-06-16 00:09:46 +0500

    hw6.1

---
+++ 06_InheritanceAndPolymorphism/hw6.1(1)/src/orderAccount/bankAccount.java
@@ -0,0 +1,34 @@
+package orderAccount;
+
+public class bankAccount {
+    double balance = 0.0;
+    public double withdrawMoney(double widthdraw){
+        if (widthdraw > balance)
+            System.out.println("Недостаточно денег на счету.");
+        else
+            balance = balance - widthdraw;
+        return balance;
+    }
+
+    public double depositMoney ( double deposit){
+        balance = balance + deposit;
+        return balance;
+    }
+
+    public double accountBalance () {
+        return balance;
+    }
+
+    public boolean send(bankAccount receiver, double amount){
+        if (this.accountBalance() < amount) {
+            System.out.println("Недостаточно денег на счету");
+            return false;
+        }
+        else{
+            this.withdrawMoney(amount);
+            receiver.depositMoney(amount);
+            System.out.println("Операция прошла успешно");
+            return true;
+        }
+    }
+}
