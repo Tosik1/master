83ff11215b3138a445e93fab633c0cf782d8894a
unknown <nikitosh1129_96@mail.ru>
2020-10-12 22:53:11 +0500

    hw9.3

--- 09_FilesAndNetwork/hw9.3/src/Loader.java
+++ 09_FilesAndNetwork/hw9.3/src/Loader.java
@@ -16,7 +16,7 @@ public class Loader {
         double sumCon = 0.0;
 
         ArrayList<Operation> op = new ArrayList<>();
-        HashSet<String> org = new HashSet<>();
+        HashMap<String, Double> org = new HashMap<>();
         ArrayList<String> lines = new ArrayList<String>();
         SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 
@@ -36,28 +36,23 @@ public class Loader {
         for (Operation operation : op) {
             sumCom += operation.getComing();
             sumCon += operation.getConsumption();
-            org.add(operation.getOrganization());
-        }
-
-        ArrayList<String> org1 = new ArrayList<>(org);
-        double[] masCon = new double[org1.size()];
-        for (int i = 0; i < org1.size(); i++) {
-            double sum = 0.0;
-            for (int a = 0; a < op.size(); a++){
-                if (org1.get(i).equals(op.get(a).getOrganization())){
-                    sum += op.get(a).getConsumption();
+            if (org.containsKey(operation.getOrganization())){
+                double n = 0.0;
+                for(Operation operation1 : op){
+                    if (operation1.getOrganization().equals(operation.getOrganization()))
+                    n += operation1.getConsumption();
                 }
+                org.put(operation.getOrganization(), n);
+            }else{
+                org.put(operation.getOrganization(), operation.getConsumption());
             }
-            masCon[i] = sum;
         }
-
 
         System.out.println("Сумма расходов: " + sumCon);
         System.out.println("Сумма доходов: " + sumCom);
         System.out.println("Сумма расходов по организациям: ");
-        for (int i = 0; i < org1.size(); i++) {
-            System.out.println("\t" + masCon[i] + "\t\t\t" + org1.get(i));
+        for (Map.Entry entry : org.entrySet()){
+            System.out.println(entry);
         }
-
     }
 }
