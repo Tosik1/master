d6e8d5053a18e3630aa8fe945c4e42b0d8ebe833
unknown <nikitosh1129_96@mail.ru>
2020-10-14 01:04:32 +0500

    hw9.4

---
+++ 09_FilesAndNetwork/hw9.4/src/Main.java
@@ -0,0 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String to = "C:\\Users\\Kita\\java_basics\\java_basics\\09_FilesAndNetwork\\hw9.4\\images";
+
+        Document doc = Jsoup.connect("https://lenta.ru").get();
+        Elements img = doc.select("img");
+        for (Element a : img) {
+            System.out.println(a.toString());
+            URL url = new URL(a.attr("abs:src"));
+            InputStream in = url.openStream();
+            Files.copy(in, Paths.get(to + "\\" + a.toString().substring
+                    (a.toString().lastIndexOf("/") + 1,
+                    a.toString().lastIndexOf(" ", a.toString().lastIndexOf(" ") - 1) - 1)), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
