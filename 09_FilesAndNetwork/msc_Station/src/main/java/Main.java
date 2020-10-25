4dca1e897cb94d797cf3873745d429c11e267a8e
unknown <nikitosh1129_96@mail.ru>
2020-10-25 15:51:16 +0500

    hw9.5

--- 09_FilesAndNetwork/msc_Station/src/main/java/Main.java
+++ 09_FilesAndNetwork/msc_Station/src/main/java/Main.java
@@ -2,86 +2,99 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-
-import javax.sound.sampled.Line;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+
 
 public class Main {
+    public static ArrayList<String> numberLines = new ArrayList<>();
+    public static JSONObject mainObj = new JSONObject();
+    public static JSONObject stations = new JSONObject();
+    public static JSONArray arrayLines = new JSONArray();
+    public static String to = "C:\\Users\\Kita\\java_basics\\java_basics\\09_FilesAndNetwork\\hw9.5\\mapmsc";
 
     public static void main(String[] args) throws IOException {
-        String to = "C:\\Users\\Kita\\java_basics\\java_basics\\09_FilesAndNetwork\\hw9.5\\mapmsc";
 
-        JSONObject mainObj = new JSONObject();
-        JSONObject stations = new JSONObject();
-        JSONArray arrayLines = new JSONArray();
+        Document doc = parseHTML("https://www.moscowmap.ru/metro.html#lines");
 
+        selectLines(doc);
 
-        Document doc = Jsoup.connect("https://www.moscowmap.ru/metro.html#lines").maxBodySize(0).get();
-        Elements lines1 = doc.select("span.js-metro-line");
-        Elements stationsTable = doc.select("div.js-metro-stations");
+        mainObj.put("stations", stations);
+        mainObj.put("lines", arrayLines);
 
-        for (Element line1 : lines1) {
-            ArrayList<String> al = new ArrayList<String>();
-            for (Element stations1 : stationsTable) {
-                if (stations1.attr("data-line").equals(line1.attr("data-line"))) {
-                    String[] text = stations1.text().split("\\d+.");
-                    for (String text1 : text) {
-                        if (text1.length() == 0) continue;
-                        al.add(text1.trim());
-                    }
+        writeToJSON(mainObj);
+        readStationsOnLine();
+    }
+
+    public static void readStationsOnLine(){
+        try {
+            JSONObject jo = (JSONObject) new JSONParser().parse(new InputStreamReader(new FileInputStream(to + "\\json.json")));
+            JSONObject countStationsObj = (JSONObject) jo.get("stations");
+
+            for (int i = 0; i < numberLines.size(); i++) {
+                JSONArray jsonArr = (JSONArray) countStationsObj.get(numberLines.get(i));
+                int count = 0;
+
+                while (count < jsonArr.size()) {
+                    count++;
                 }
+                System.out.println("Количество станций на " + numberLines.get(i) + " линии составляет: " + count);
             }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void selectLines(Document doc){
+        Elements lines = doc.select("span.js-metro-line");
+        Elements stationsTable = doc.select("div.js-metro-stations");
+        for (Element line1 : lines) {
+            ArrayList<String> al = new ArrayList<String>();
+            numberLines.add(line1.attr("data-line"));
+
+            selectStations(stationsTable, line1, al);
+
             stations.put(line1.attr("data-line"), al);
             JSONObject lines2 = new JSONObject();
             lines2.put("number", line1.attr("data-line"));
             lines2.put("name", line1.text());
             arrayLines.add(lines2);
         }
-        mainObj.put("stations", stations);
-        mainObj.put("lines", arrayLines);
+    }
 
+    public static void selectStations(Elements stations, Element line1, ArrayList<String> masStations){
+        for (Element stations1 : stations) {
+            if (stations1.attr("data-line").equals(line1.attr("data-line"))) {
+                String[] text = stations1.text().split("\\d+.");
+                for (String text1 : text) {
+                    if (text1.length() == 0) continue;
+                    masStations.add(text1.trim());
+                }
+            }
+        }
+    }
+
+    public static Document parseHTML(String site) throws IOException {
+        Document document = Jsoup.connect(site).maxBodySize(0).get();
+        return document;
+    }
+
+    public static void writeToJSON(JSONObject obj){
         try {
             FileWriter file = new FileWriter(to + "\\" + "json.json");
 
-            file.write(mainObj.toString());
+            file.write(obj.toString());
             file.flush();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-//Чтение и вывод в консоль количество станций на каждой линии
-        ArrayList<String> asd = new ArrayList<String>(stations.keySet());
-        try {
-            JSONObject jo = (JSONObject) new JSONParser().parse(new InputStreamReader(new FileInputStream(to + "\\json.json")));
-            JSONObject countStationsObj = (JSONObject) jo.get("stations");
-
-            for (int i = 0; i < asd.size(); i++) {
-                JSONArray jsonArr = (JSONArray) countStationsObj.get(asd.get(i));
-                int count = 0;
-
-                while (count < jsonArr.size()) {
-                    count++;
-                }
-                System.out.println("Количество станций на " + asd.get(i) + " линии составляет: " + count);
-            }
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
         }
     }
 }
