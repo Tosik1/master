0dfcd53f57c518819fd9a6d26cdacc95b1e577fd
unknown <nikitosh1129_96@mail.ru>
2020-11-07 01:07:49 +0500

    hw10.2

--- 10_SQLAndHibernate/hw10.1/src/main/java/Main.java
+++ 10_SQLAndHibernate/hw10.1/src/main/java/Main.java
@@ -1,51 +1,24 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox";
-        String user = "root";
-        String pass = "Qazwsx12344321";
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
-            Statement statement0 = connection.createStatement();
-            ResultSet resultSet0 = statement0.executeQuery("SELECT course_name  FROM PurchaseList group by course_name;");
-            while(resultSet0.next()){
-                String courseName = resultSet0.getString("course_name");
-                int countSub = 0;
-                int countMaxMonth = 0;
-                int countMinMonth = 0;
-                ArrayList<Integer> al = new ArrayList<Integer>();
-                for (int i = 1; i < 13; i++) {
-                    Statement statement = connection.createStatement();
+        Session session = sessionFactory.openSession();
 
-                    ResultSet resultSet = statement.executeQuery("SELECT course_name, subscription_date  FROM PurchaseList " +
-                            "WHERE course_name = '" + courseName +
-                            "' AND MONTH(subscription_date) = " + i + ";");
-                    while (resultSet.next()) {
-                        String columnName = resultSet.getString(2);
+        //Указание id курса.
+        int id = 1;
+        Course course = session.get(Course.class, id);
+        System.out.println(course.getName());
+        System.out.println(course.getStudentsCount());
 
-                        if (columnName != null) {
-                            al.add(i);
-                            countSub++;
-                            countMaxMonth = i;
-                        }
-                    }
-                    statement.close();
-                    resultSet.close();
-                }
-                System.out.println("Среднее количество покупок курса \"" + courseName + "\" в месяц за 2018: " + (double) countSub / (countMaxMonth - al.get(0)));
-            }
-            statement0.close();
-            resultSet0.close();
-            connection.close();
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
+        sessionFactory.close();
     }
 }
