285db5e1771000bf56b0e78952b4fdb1a66c3e53
unknown <nikitosh1129_96@mail.ru>
2020-11-15 18:00:01 +0500

    hw11.2

---
+++ 11_Multithreading/Transactions/src/main/java/Bank.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class Bank
+{
+    private HashMap<String, Account> accounts;
+    private final Random random = new Random();
+
+    Bank(HashMap<String, Account> map){
+        this.accounts = map;
+    }
+
+    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+        throws InterruptedException
+    {
+        Thread.sleep(1000);
+        return random.nextBoolean();
+    }
+
+    public void transfer(String fromAccountNum, String toAccountNum, long amount, HashMap<String, Account> accounts)
+    {
+        this.accounts = accounts;
+        if (amount > accounts.get(fromAccountNum).getMoney()){
+            System.out.println("Недостаточно средств для перевода");
+        }else {
+            try {
+                if (isFraud(fromAccountNum, toAccountNum, amount)) {
+                    System.out.println("Счета " + fromAccountNum + ", " + toAccountNum + " заблокированы, обнаружено мошенничество.");
+                    accounts.remove(fromAccountNum);
+                    accounts.remove(toAccountNum);
+                }
+                else {
+                    Account from = accounts.get(fromAccountNum);
+                    Account to = accounts.get(toAccountNum);
+
+                    from.setMoney(from.getMoney() - amount);
+                    to.setMoney(to.getMoney() + amount);
+                    System.out.println(amount + " руб. со счета " + fromAccountNum + " переведены на счет " + toAccountNum);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public long getBalance(String accountNum)
+    {
+        return accounts.get(accountNum).getMoney();
+    }
+
+    public HashMap<String, Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(HashMap<String, Account> accounts) {
+        this.accounts = accounts;
+    }
+}
---
+++ 11_Multithreading/Transactions/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Account> mapAccount;
+        Bank bankTest;
+        mapAccount = new HashMap<String, Account>();
+        for (int i = 0; i < 20; i++) {
+            mapAccount.put(Integer.toString(i), new Account((long) (Math.random() * 1000000 + 50000), Integer.toString(i)));
+        }
+
+        bankTest = new Bank(mapAccount);
+
+        for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+            int finalI = i;
+            new Thread(() -> {
+//                for (int j = 0; j < mapAccount.size(); j += 2)
+                    bankTest.transfer(mapAccount.get(Integer.toString(finalI)).getAccNumber(),
+                            mapAccount.get(Integer.toString(finalI + 1)).getAccNumber(),
+                            60000, mapAccount);
+            }).start();
+        }
+    }
+}
