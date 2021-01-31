04aeacf5724a5337c3ddb038e1078be86f0b66c1
unknown <nikitosh1129_96@mail.ru>
2021-01-31 17:41:07 +0500

    try2

--- 10_SQLAndHibernate/ForkJoin/src/main/java/Main.java
+++ 10_SQLAndHibernate/ForkJoin/src/main/java/Main.java
@@ -2,12 +2,13 @@ import java.util.HashSet;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+
+    static HashSet<String> listSites = new HashSet<>();
+
     public static void main(String[] args) {
         String sites = "https://skillbox.ru";
-        HashSet<String> allSites = new HashSet<>();
-        allSites.add(sites);
-//        allSites.addAll(new ForkJoinPool().invoke(new SiteAdress(allSites)));
-        HashSet<String> qwe = new HashSet<>(new ForkJoinPool().invoke(new SiteAdress(allSites)));
+        listSites.add(sites);
+        HashSet<String> qwe = new HashSet<>(new ForkJoinPool().invoke(new SiteAdress(listSites)));
         for (String i : qwe){
             System.out.println(i);
         }
--- 10_SQLAndHibernate/ForkJoin/src/main/java/SiteAdress.java
+++ 10_SQLAndHibernate/ForkJoin/src/main/java/SiteAdress.java
@@ -5,50 +5,38 @@ import java.util.concurrent.RecursiveTask;
 
 public class SiteAdress extends RecursiveTask<HashSet> {
 
-    private HashSet<String> listSites = new HashSet<>();
-    private HashSet<String> sites;
-    private ArrayList<String> control = new ArrayList<>();
+    private HashSet<String> listSites;
 
-    private String fullMap;
-
-    public SiteAdress(HashSet<String> sites){
-        this.sites = sites;
+    public SiteAdress(HashSet<String> listSites){
+        this.listSites = listSites;
     }
 
     @Override
     protected HashSet<String> compute() {
-        List<SiteAdress> taskList = new ArrayList<>();
-        control.addAll(sites);
+        HashSet<SiteAdress> taskList = new HashSet<>();
         try {
-            for (String site : sites) {
-                listSites = Parser.parseHTML(site);
-                for (String str : control){
-                    for (String str1 : listSites) {
-                        if (!str1.equals(str)) {
-                            SiteAdress task = new SiteAdress(listSites);
-                            task.fork();
-                            taskList.add(task);
-                            control.add(site);
-                        }
-                    }
-                }
+            for (String str1 : listSites) {
+                SiteAdress task = new SiteAdress(Parser.parseHTML(str1));
+                task.fork();
+                taskList.add(task);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (SiteAdress task : taskList){
-            sites.addAll(task.join());
-            try {
-                task.wait(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            listSites.addAll(task.join());
+
+//            try {
+//                task.wait(200);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
 
-        return sites;
+        return listSites;
     }
 
     public HashSet<String> getSites() {
-        return sites;
+        return listSites;
     }
 }
