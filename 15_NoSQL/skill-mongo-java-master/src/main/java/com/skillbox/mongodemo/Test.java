0a8838dd6b1c1eb85ad283b36cfbe8d78b35edf1
unknown <nikitosh1129_96@mail.ru>
2021-04-17 00:40:57 +0500

    hw15.3

--- 15_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
+++ 15_NoSQL/skill-mongo-java-master/src/main/java/com/skillbox/mongodemo/Test.java
@@ -1,22 +1,22 @@
 package com.skillbox.mongodemo;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.ListDatabasesIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.BsonDocument;
 import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.json.JsonMode;
-import org.bson.json.JsonWriterSettings;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.function.Consumer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Test {
 
+    private static final String filePath = "C:\\Users\\Kita\\java_basics\\java_basics\\15_NoSQL\\skill-mongo-java-master\\mongo.csv";
+
     public static void main(String[] args) {
+
         MongoClient mongoClient = new MongoClient( "127.0.0.1" , 27017 );
 
         MongoDatabase database = mongoClient.getDatabase("local");
@@ -27,39 +27,36 @@ public class Test {
         // Удалим из нее все документы
         collection.drop();
 
-        // Создадим первый документ
-        Document firstDocument = new Document()
-                .append("Type", 1)
-                .append("Description", "Это наш первый документ в MongoDB")
-                .append("Author", "Я")
-                .append("Time", new SimpleDateFormat().format(new Date()));
+        ArrayList<String> array = new ArrayList<>();
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //Записали всех студентов в массив
+                array.add(line);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        // Вложенный объект
-        Document nestedObject = new Document()
-                .append("Course", "NoSQL Базы Данных")
-                .append("Author", "Mike Ovchinnikov");
+        //добавляем каждого студента в коллекцию
+        for (String student : array){
+            String[] category = student.split(",", 3);
+            Document doc = new Document().append("name", category[0]).append("age", category[1]).append("courses", category[2]);
+            collection.insertOne(doc);
+        }
 
-        firstDocument.append("Skillbox", nestedObject);
+// общее количество студентов в базе.
+// количество студентов старше 40 лет.
+// имя самого молодого студента.
+// список курсов самого старого студента.
 
-
-        // Вставляем документ в коллекцию
-        collection.insertOne(firstDocument);
-
-        collection.find().forEach((Consumer<Document>) document -> {
-            System.out.println("Наш первый документ:\n" + document);
-        });
-
-        // Используем JSON-синтаксис для создания объекта
-        Document secondDocument = Document.parse(
-                "{Type: 2, Description:\"Мы создали и нашли этот документ с помощью JSON-синтаксиса\"}"
-        );
-        collection.insertOne(secondDocument);
+        // Используем JSON-синтаксис для написания запроса (выводим общее количество студентов)
+        System.out.println(collection.countDocuments());
 
         // Используем JSON-синтаксис для написания запроса (выбираем документы с Type=2)
-        BsonDocument query = BsonDocument.parse("{Type: {$eq: 2}}");
-        collection.find(query).forEach((Consumer<Document>) document -> {
-            System.out.println("Наш второй документ:\n" + document);
-        });
+        BsonDocument query = BsonDocument.parse("{ age: { $gte: 40 } }");
+        collection.find(query).?????;
     }
 }
